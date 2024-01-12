5908968ce5633820dc02c6b7fc3c0907dfcf6a45
Kuan wei su <91251074+a344566225@users.noreply.github.com>
2024-01-12 17:48:17 +0800

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int mile;
+    scanf("%d", &mile);
+    double km = mile * 1.6;
+    printf("%.1f\n", km);
+}
+    
